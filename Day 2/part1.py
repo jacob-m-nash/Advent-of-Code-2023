d27e9971669f308da25265e1ec5a2a81fb4fad58
jacob-m-nash <44061160+jacob-m-nash@users.noreply.github.com>
2023-12-02 13:43:33 +0000

    Day 2 part 1

---
+++ Day 2/part1.py
@@ -0,0 +1,31 @@
+validGames = []
+MAX_RED_CUBES = 12
+MAX_GREEN_CUBES = 13
+MAX_BLUE_CUBES = 14
+
+def GameValidator(pulls):
+    for pull in pulls.split(";"):
+            for cube in pull.split(","):
+                cubeCount,cubeColour = cube.split()
+                if(cubeColour == "red"):
+                     if(int(cubeCount) > MAX_RED_CUBES):
+                          return False
+                if(cubeColour == "green"):
+                     if(int(cubeCount) > MAX_GREEN_CUBES):
+                          return False
+                if(cubeColour == "blue"):
+                     if(int(cubeCount) > MAX_BLUE_CUBES):
+                          return False
+    return True
+                 
+
+with open("data.txt") as input:
+    for line in input:
+        impossibleFlag = False
+        line = line.rstrip()
+        game,pulls = line.split(":")
+        gameNumber = int(game.split()[1])
+        if(GameValidator(pulls) != False):
+             validGames.append(gameNumber)
+        
+print(f"Sum of valid games: {sum(validGames)}")
