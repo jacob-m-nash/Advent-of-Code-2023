e6de946411fb7de6bc0bd412c211b4eed48bdb1c
Jacob Nash <jacob.matthew.nash@gmail.com>
2023-12-26 15:03:51 +0000

    Day 5 part 1

--- Day 5/part1.py
+++ Day 5/part1.py
@@ -1,34 +1,54 @@
-def calcMapping(line,sources):
-    
-    destination_range_start,source_range_start, range_length = line.split()
-    max = source_range_start + range_length
-    for source in sources:
+def MapSource(source, map):
+    for line in map:
+        destination_range_start,source_range_start, range_length = line.split()
+        max = int(source_range_start) + (int(range_length) - 1) # Range incudes starting number so  -1
         if source >= int(source_range_start) and source < int(max):
             diff = source - int(source_range_start)
             return (int(destination_range_start) + diff)
+    # If no mapping return original source
+    return(source)
 
-
-
-with open("data.txt") as input:
-    _,seeds = input.readline().split(":")
-    seeds = seeds.split()
-    seeds = [int(item) for item in seeds]
-    print(seeds)
-
-
+def readMap(sources, input):
+    # Skipping over blank lines to get to the start of the Map
     for line in input:
         if line.strip():
             break
-    # TODO: auto go through maps
-    # seed-to-soil map
-    soils = []
-    i = 0
+
+    map = []
+    output = []
     for line in input:
         if line.strip():
-            soils.append(calcMapping(line,seeds))
+            map.append(line.strip())
         else:
             break
-    print(soils)
+    for source in sources:
+        output.append(MapSource(source,map))
+
+    return output
+
+def Main():
+    with open("data.txt") as input:
+        _,seeds = input.readline().split(":")
+        seeds = seeds.split()
+        seeds = [int(item) for item in seeds]
+
+        # Seeds to soil
+        soil = readMap(seeds,input)
+        # Soil to fertilizer 
+        fertilizer = readMap(soil,input)
+        # Fertilizer to water
+        water = readMap(fertilizer,input)
+        # Water to light
+        light = readMap(water,input)
+        # Light to temperature
+        temperature = readMap(light,input)
+        # Temperature to humidity
+        humidity = readMap(temperature,input)
+        # Humidity to location
+        location = readMap(humidity,input)
+        print(f"Min location: {min(location)}")
 
 
-
+if __name__ == "__main__":
+    Main()
+    
